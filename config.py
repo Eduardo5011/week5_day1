320de365359e95512a6629b7288231ca46c79c8f
Eduardo5011 <eduardo.rosado5011@gmail.com>
2021-10-27 18:04:47 -0400

    first commit

---
+++ config.py
@@ -0,0 +1,6 @@
+class Config():
+    REGISTERED_USERS = {
+    'Eros@example.com':{"name":"Eduardo","password":"pumkin"},
+    'Ltav@example.com':{"name":"Livael","password":"brown"},
+    'Gmol@example.com':{"name":"Guy","password":"green"}
+    }
