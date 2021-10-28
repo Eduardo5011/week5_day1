2422b4587a06d08467f70c18efa394a2f3ac475f
Eduardo5011 <eduardo.rosado5011@gmail.com>
2021-10-28 17:59:14 -0400

    update

--- app/routes.py
+++ app/routes.py
@@ -1,16 +1,22 @@
-from flask import render_template, request
+from flask import render_template, request, redirect, url_for
 import requests
 from app import app
+from .forms import LoginForm, RegisterForm, PokeForm
+from . models import User
+from flask_login import login_user, current_user, logout_user, login_required
+
 
 
 
 
 @app.route("/", methods = ['GET'])
+@login_required
 def index():
     return render_template('index.html.j2')
 
 
 @app.route('/pokemon', methods = ['GET'])  
+@login_required
 def pokemon():
     my_pokemon = ["pikachu", "charizard", "mudkip", "blaziken", "cubone"]
 
@@ -18,15 +24,48 @@ def pokemon():
 
 @app.route('/login', methods=['GET', 'POST'])  
 def login():
-    if request.method == 'POST':
+    form = LoginForm()
+    if request.method == 'POST' and form.validate_on_submit():
         email = request.form.get("email").lower()
         password = request.form.get("password")
-        if email in app.config.get("REGISTERED_USERS") and password == app.config.get("REGISTERED_USERS").get(email).get('password'):
-            return f"Login success Welcome {app.config.get('REGISTERED_USERS')(email).get('name')}"
-        error_string = "Invalid Email password combo"    
-        return render_template('login.html.j2', error = error_string)
+        u = User.query.filter_by(email=email).first()
 
-    return render_template('login.html.j2')
+        if u and u.check_hashed_password(password):
+            login_user(u)
+            return redirect(url_for("index"))
+        error_string = "Invalid Email password combo"    
+        return render_template('login.html.j2', error = error_string, form=form)
+    return render_template('login.html.j2', form=form)
+
+@app.route('/logout')   
+def logout():
+    if current_user:
+        logout_user()
+        return redirect(url_for('login'))
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+    if request.method == 'POST' and form.validate_on_submit():
+        try:
+            new_user_data = {
+                "first_name":form.first_name.data.title(),
+                "last_name":form.last_name.data.title(),
+                "email":form.email.data.lower(),
+                "password": form.password.data
+            }
+            
+            new_user_object = User()
+            
+            new_user_object.from_dict(new_user_data)
+            
+            new_user_object.save()
+        except:
+            error_string = "There was an unexpected Error creating your account. Please Try again."
+            return render_template('register.html.j2',form=form, error = error_string) 
+        return redirect(url_for('login')) 
+    return render_template('register.html.j2', form = form) 
 
 
 
@@ -34,7 +73,8 @@ def login():
 
 @app.route('/pokeapi', methods=['GET', 'POST'])  
 def pokeapi():
-    if request.method == 'POST':
+    form = PokeForm()
+    if request.method == 'POST' and form.validate_on_submit():
         names = request.form.get('name')
         url = f"https://pokeapi.co/api/v2/pokemon/{names}"
         response_name = requests.get(url)
@@ -52,13 +92,13 @@ def pokeapi():
             }
             poke_info.append(poke_team)   
             print(poke_info) 
-            return render_template('pokeapi.html.j2', information=poke_info)
+            return render_template('pokeapi.html.j2', information=poke_info, form=form)
 
         else:
             
-            return " Houston we had a problem "   
+            return " Houston we have a problem "   
 
-    return render_template('pokeapi.html.j2')
+    return render_template('pokeapi.html.j2', form=form)
         
         
        
--- config.py
+++ config.py
@@ -1,6 +1,11 @@
+import os
+
 class Config():
     REGISTERED_USERS = {
-    'Eros@example.com':{"name":"Eduardo","password":"pumkin"},
-    'Ltav@example.com':{"name":"Livael","password":"brown"},
-    'Gmol@example.com':{"name":"Guy","password":"green"}
+    'eros@gmail.com':{"name":"Eduardo","password":"123"},
+    'ltav@gmail.com':{"name":"Livael","password":"abc"},
+    'gmol@gmail.com':{"name":"Guy","password":"qwerty"}
     }
+    SECRET_KEY = os.environ.get("SECRET_KEY") or "you-will-never-guess"
+    SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI")
+    SQLALCHEMY_TRACK_MODIFICATIONS = os.environ.get("SQLALCHEMY_TRACK_MODIFICATIONS")
